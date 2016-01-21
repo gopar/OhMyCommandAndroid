7a79813fa947e29c71502df61aa3f5a4979ef008
Daniel Gopar <gopardaniel@yahoo.com>
2016-01-20 21:05:18 -0800

    Signup page created and styled.

--- app/src/main/java/com/pygopar/ohmycommand/LoginActivity.java
+++ app/src/main/java/com/pygopar/ohmycommand/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.pygopar.ohmycommand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.link_signup)
+    public void onClickLinkSignup() {
+        startActivity(new Intent(getApplicationContext(), SignupActivity.class));
     }
 }
