5d83b833d8fd8d69ab3a3a712567d142ab56229a
Daniel Gopar <gopardaniel@yahoo.com>
2016-01-20 21:09:29 -0800

    Login and Signup now link to each other

--- app/src/main/java/com/pygopar/ohmycommand/SignupActivity.java
+++ app/src/main/java/com/pygopar/ohmycommand/SignupActivity.java
@@ -3,11 +3,20 @@ package com.pygopar.ohmycommand;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.link_login)
+    public void onClickLinkLogin() {
+        finish();
     }
 }
